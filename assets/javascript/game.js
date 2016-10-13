4a68df71a277058db34ff58c87d98bf78bbee8e7
Our Laptop <Our Laptop>
2016-10-13 02:19:19 -0700

    Updates to logic - correctly displaying user input, adding wins and restarting turn.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,11 +6,8 @@ var randomWords;
 var rightGuess = [];
 var userGuess = [];
 var userInput;
-
-
-
 // randomly select a word for user to play
-wordsToPlay = ["RODEO", "MULHOLLAND", "HOLLYWOOD", "SUNSET", "MELROSE"];
+wordsToPlay = ["RODEO", "MULHOLLAND", "HOLLYWOOD", "SUNSET", "MELROSE", ];
 randomWords = wordsToPlay[Math.floor(Math.random() * wordsToPlay.length)];
 hiddenWords = randomWords.split("");
 // replace all characters in the array with an "_"
@@ -21,54 +18,47 @@ for (var i = 0; i < hiddenWords.length; i++) {
 document.getElementById("the-word").innerHTML = (hiddenWords.join(""));
 // Capture user input 
 document.onkeyup = function() {
-        userInput = String.fromCharCode(event.keyCode).toUpperCase();      
-        userGuess.push(userInput);
-
-
-// compare user input against the word solution, if correct then reveal the letter if incorrect then reduce guesses left by 1
-            for(var j=0; j<randomWords.length; j++) {
-                if(randomWords.charAt(j) === userInput) {
-                    rightGuess.push(userInput);
-                    hiddenWords.splice(j,1,userInput);
-                }
-                else if (randomWords.charAt(j) !== userInput) {
-                    guessesLeft --;
-                }
-            }
-        
-            document.getElementById("already-guessed").innerHTML = userGuess;
-            document.getElementById("the-word").innerHTML = (hiddenWords.join(""));
-            document.getElementById("guesses-remaining").innerHTML =
-                guessesLeft;
-// if user wins then add 1 to wins
-
-    
-if (hiddenWords.join("").toString() === randomWords) {
-                correctAnswer++;
-                nextTurn();
-}
-
-            document.getElementById("user-wins").innerHTML = correctAnswer;
-// new game
-// if user wins or losses then choose another word automatically to play
-function nextTurn(){
-    guessesLeft = 5;
-    userGuess = [];
-randomWords = wordsToPlay[Math.floor(Math.random() * wordsToPlay.length)];
-hiddenWords = randomWords.split("");
-for (var i = 0; i < hiddenWords.length; i++) {
-    hiddenWords[i] = "_ ";
-}
-document.getElementById("the-word").innerHTML = (hiddenWords.join(""));
-}
-    
-// replace image to correspond with word
-    if (hiddenWords.join("") === "RODEO") {
-    function changeImage() {
-        document.getElementByClassName("img-responsive").src = "../images/Rodeo.jpg";
+    userInput = String.fromCharCode(event.keyCode).toUpperCase();
+    userGuess.push(userInput);
+    // compare user input against the word solution, if correct then reveal the letter if incorrect then reduce guesses left by 1
+    for (var j = 0; j < randomWords.length; j++) {
+        if (randomWords.charAt(j) === userInput) {
+            rightGuess.push(userInput);
+            hiddenWords.splice(j, 1, userInput);
+        } else if (randomWords.charAt(j) !== userInput) {
+            guessesLeft--;
+        }
     }
-    
-    }   
-    
-    };
-    
+    document.getElementById("already-guessed").innerHTML = userGuess;
+    document.getElementById("the-word").innerHTML = (hiddenWords.join(""));
+    document.getElementById("guesses-remaining").innerHTML = guessesLeft;
+    // if user wins then add 1 to wins
+    if (hiddenWords.join("").toString() === randomWords) {
+        correctAnswer++;
+        nextTurn();
+    }
+    document.getElementById("user-wins").innerHTML = correctAnswer;
+    // new game
+    // if user wins or losses then choose another word automatically to play
+    function nextTurn() {
+            guessesLeft = 5;
+            userGuess = [];
+            randomWords = wordsToPlay[Math.floor(Math.random() *
+                wordsToPlay.length)];
+            hiddenWords = randomWords.split("");
+            for (var i = 0; i < hiddenWords.length; i++) {
+                hiddenWords[i] = "_ ";
+            }
+            document.getElementById("the-word").innerHTML = (hiddenWords.join(
+                ""));
+        }
+        // replace image to correspond with word
+
+    function changeImage() {
+        document.getElementByClassName("img-responsive").src =
+            "../images/Rodeo.jpg";
+    }
+    if (hiddenWords.join("") === "RODEO") {
+        changeImage();
+    }
+};
